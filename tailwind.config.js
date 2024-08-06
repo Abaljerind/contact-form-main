3c9a690cb2157406626e307bc7317148d710d0d8
AbalJerind <abaljerinda@gmail.com>
2024-08-06 14:09:42 +0700

    add animation & keyframes for fade-in & fade-out

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
         "grey-md": "#86a2a6",
         "grey-xl": "#2a4245",
       },
+      animation: {
+        "fade-in": "fade-in 1s forwards",
+        "fade-out": "fade-out 1s forwards",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "fade-out": {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
     },
   },
   plugins: [],
