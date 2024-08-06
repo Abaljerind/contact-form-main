948a31674291f0a5eb664f360f90612794f781eb
AbalJerind <abaljerinda@gmail.com>
2024-08-06 14:14:10 +0700

    add condition for submit form manually & animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 /* list yang belum selesai: 
 1. error-state [DONE]
-2. desktop-view
-3. success-state
+2. desktop-view [DONE]
+3. success-state [DONE]
 */
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -24,14 +24,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const invalidMessage = document.getElementById("messageInvalid");
   const invalidAgree = document.getElementById("agreeInvalid");
 
+  const successState = document.getElementById("success-state");
+
   // form event
   contactForm.addEventListener("submit", function (e) {
     e.preventDefault();
+
+    let isValid = true;
 
     if (firstName.value.length === 0 || !isNaN(firstName.value)) {
       invalidFirstName.classList.remove("hidden");
       firstName.classList.remove("valid-input");
       firstName.classList.add("invalid-input");
+      isValid = false;
     } else {
       invalidFirstName.classList.add("hidden");
       firstName.classList.add("valid-input");
@@ -42,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
       invalidLastName.classList.remove("hidden");
       lastName.classList.remove("valid-input");
       lastName.classList.add("invalid-input");
+      isValid = false;
     } else {
       invalidLastName.classList.add("hidden");
       lastName.classList.add("valid-input");
@@ -53,6 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
       invalidEmail.classList.remove("hidden");
       emailAddress.classList.remove("valid-input");
       emailAddress.classList.add("invalid-input");
+      isValid = false;
     } else {
       invalidEmail.classList.add("hidden");
       emailAddress.classList.add("valid-input");
@@ -63,12 +70,14 @@ document.addEventListener("DOMContentLoaded", function () {
       invalidQueryType.classList.add("hidden");
     } else {
       invalidQueryType.classList.remove("hidden");
+      isValid = false;
     }
 
     if (message.value.length === 0) {
       invalidMessage.classList.remove("hidden");
       message.classList.remove("valid-input");
       message.classList.add("invalid-input");
+      isValid = false;
     } else {
       invalidMessage.classList.add("hidden");
       message.classList.add("valid-input");
@@ -79,6 +88,25 @@ document.addEventListener("DOMContentLoaded", function () {
       invalidAgree.classList.add("hidden");
     } else {
       invalidAgree.classList.remove("hidden");
+      isValid = false;
+    }
+
+    if (isValid) {
+      // munculkan modal jika isValid = true
+      successState.classList.remove("hidden");
+
+      setTimeout(() => {
+        successState.classList.remove("animate-fade-in");
+        successState.classList.add("animate-fade-out");
+      }, 3000);
+
+      setTimeout(() => {
+        successState.classList.add("hidden");
+        successState.classList.remove("animate-fade-out");
+
+        // melakukan submit secara manual setelah modal disembunyikan.
+        contactForm.submit();
+      }, 5000);
     }
   });
 });
